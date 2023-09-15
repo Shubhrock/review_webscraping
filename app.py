1ffa53b09092e575cc4e21a844881068427a6171
Shubhrock <rastogishubhankar102@gmail.com>
2023-09-15 17:44:01 +0530

    checking any changed in the file

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ import urllib
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
+@cross_origin()
 def homepage():
     return render_template('index.html')
 
 @app.route('/review', methods = ['POST', 'GET'])
+@cross_origin()
 def index():
     if request.method == 'POST':
         try:
