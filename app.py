d5d71fb0e9219d05126d8fcc61e0156bfd62e658
Shubhrock <rastogishubhankar102@gmail.com>
2023-09-15 17:11:42 +0530

    first commit

--- app.py
+++ app.py
@@ -1,50 +1,53 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+logging.basicConfig(filename="scrapper.log", level=logging.INFO)
+import collections
+from pymongo.mongo_client import MongoClient
+import urllib
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
+@app.route('/', methods = ['GET'])
 def homepage():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+@app.route('/review', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(' ', '')
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            flipkart_html = bs(flipkartPage, 'html.parser')
+            bigboxes = flipkart_html.findAll('div', {'class' : '_1AtVbE col-12-12'})
             del bigboxes[0:3]
             box = bigboxes[0]
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
+            prodRes.encoding = 'utf-8'
+            prod_html = bs(prodRes.text, 'html.parser')
             print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            commentboxes = prod_html.find_all('div', {'class':'_16PBlm'})
 
             filename = searchString + ".csv"
             fw = open(filename, "w")
             headers = "Product, Customer Name, Rating, Heading, Comment \n"
             fw.write(headers)
             reviews = []
+            #logging.info(commentboxes)
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
                     name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
                 except:
                     logging.info("name")
-
+                
                 try:
                     #rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
@@ -72,6 +75,13 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            pass1 = urllib.parse.quote("password@3001")
+            uri = "mongodb+srv://shubh:{}@cluster0.sne4rgf.mongodb.net/?retryWrites=true&w=majority".format(pass1)
+            client = MongoClient(uri)
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
+            
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
